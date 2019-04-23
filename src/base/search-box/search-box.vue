382bcdf834e2eac116671dcca5a17d14a268bb5e
wenxingjie <wen_xingjie@163.com>
2019-04-24 07:18:55 +0800

    完成 10-3 搜索页面热门搜索数据抓取和应用 的学习

---
+++ src/base/search-box/search-box.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="search-box">
+    <i class="icon-search"></i>
+    <input type="text"
+           class="search-input"
+           v-model="query"
+           :placeholder="placeholder">
+    <i @click="clear"
+       v-show="query"
+       class="icon-dismiss"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+      default: '搜索歌曲、歌手'
+    }
+  },
+  data () {
+    return {
+      query: ''
+    }
+  },
+  created () {
+    this.$watch('query', (newVal) => {
+      this.$emit('query', this.query)
+    })
+  },
+  methods: {
+    clear () {
+      this.query = ''
+    },
+    setQuery (query) {
+      this.query = query
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styl/variable'
+.search-box
+  display: flex
+  height: 40px
+  padding: 0 6px
+  align-items: center
+  background: $color-highlight-background
+  border-radius: 6px
+  .icon-search
+    font-size: 24px
+    color: $color-dialog-background
+  .search-input
+    flex: 1
+    margin: 0 5px
+    line-height: 18px
+    background: $color-highlight-background
+    color: $color-text
+    font-size: $font-size-medium
+    outline: 0
+  .icon-dismiss
+    font-size: $font-size-medium-x
+    color: $color-dialog-background
+</style>
--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -1,15 +1,79 @@
 <template>
-  <div>
-搜索页面
+  <div class="search-wrap">
+    <div class="search-box-wrap">
+      <search-box @query="onquery" ref="searchBox"></search-box>
+    </div>
+    <div class="shortcut-wrap">
+      <div>
+        <div class="hot-key">
+          <h2 class="title">热门搜索</h2>
+          <ul class="hot-key-list">
+            <li class="hot-item" v-for="(item,index) in hotKeys" :key="index" @click="addQuery(item.k)">{{item.k}}</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import SearchBox from 'base/search-box/search-box'
+import { getHotKey } from 'api/search'
+import { ERR_OK } from 'api/configs'
 
+export default {
+  components: {
+    SearchBox
+  },
+  data: function () {
+    return {
+      query: '',
+      hotKeys: []
+    }
+  },
+  created () {
+    this._getHotKey()
+  },
+  methods: {
+    onquery (query) {
+      this.query = query
+    },
+    addQuery (query) {
+      this.$refs.searchBox.setQuery(query)
+    },
+    _getHotKey () {
+      getHotKey().then(res => {
+        if (res.code === ERR_OK) {
+          this.hotKeys = res.data.hotkey.slice(0, 10)
+        }
+      })
+    }
+  }
 }
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+@import '~styl/variable'
+.search-wrap
+  .search-box-wrap
+    margin: 20px
+  .shortcut-wrap
+    position: fixed
+    top: 168px
+    bottom: 0
+    .hot-key
+      margin: 0 20px 20px 20px
+      .title
+        margin-bottom: 20px
+        font-size: $font-size-medium
+        color: $color-text-l
+      .hot-key-list
+        .hot-item
+          display: inline-block
+          padding: 5px 10px
+          margin: 0 20px 10px 0
+          border-radius: 6px
+          background: #333
+          font-size: $font-size-medium
+          color: $color-text-d
 </style>
